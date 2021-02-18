8649b174594d560a73bde6136eb56b7a85ac1e01
SunGuk <sgk0713@estsoft.com>
2021-02-19 03:07:52 +0900

    usecase 작성

---
+++ app/src/main/java/com/evangers/stockfield/domain/usecase/GetFund.kt
@@ -0,0 +1,27 @@
+package com.evangers.stockfield.domain.usecase
+
+import com.evangers.stockfield.domain.model.FundModel
+import com.evangers.stockfield.domain.repository.FundRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetFund @Inject constructor(
+    private val fundRepositoryImpl: FundRepository
+) : FlowUseCase<GetFund.Request, GetFund.Response> {
+    override suspend fun invoke(request: Request): Flow<Response> = flow {
+        try {
+            val fund = fundRepositoryImpl.getFund(request.fundName)
+            emit(Response.Success(fund))
+        } catch (e: Exception) {
+            emit(Response.Failure(e.message.toString()))
+        }
+    }
+
+    class Request(val fundName: String)
+
+    sealed class Response {
+        class Success(val fund: FundModel) : Response()
+        class Failure(val errorMessage: String) : Response()
+    }
+}
