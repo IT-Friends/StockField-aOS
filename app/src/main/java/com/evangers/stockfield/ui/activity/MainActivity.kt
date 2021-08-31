4a1d55d57640d8170460d3e524c13b926a1697d7
SunGuk <sgk0713@gmail.com>
2021-09-01 01:10:45 +0900

    애드몹 초기화 추가

--- app/src/main/java/com/evangers/stockfield/ui/activity/MainActivity.kt
+++ app/src/main/java/com/evangers/stockfield/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.evangers.stockfield.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.evangers.stockfield.R
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
     }
 }
