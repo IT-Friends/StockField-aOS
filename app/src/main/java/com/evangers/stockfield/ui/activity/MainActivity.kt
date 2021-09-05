995f2336a8f611a12a2c4aa55aebdf3292fd1815
SunGuk <sgk0713@gmail.com>
2021-09-05 21:53:33 +0900

    admob init applicatino 으로 이동

--- app/src/main/java/com/evangers/stockfield/ui/activity/MainActivity.kt
+++ app/src/main/java/com/evangers/stockfield/ui/activity/MainActivity.kt
@@ -3,7 +3,6 @@ package com.evangers.stockfield.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.evangers.stockfield.R
-import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,7 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        MobileAds.initialize(this) {}
     }
 }
--- app/src/main/java/com/evangers/stockfield/ui/di/StockFieldApp.kt
+++ app/src/main/java/com/evangers/stockfield/ui/di/StockFieldApp.kt
@@ -1,7 +1,14 @@
 package com.evangers.stockfield.ui.di
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class StockFieldApp : Application()
+class StockFieldApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        MobileAds.initialize(this) {}
+    }
+}
