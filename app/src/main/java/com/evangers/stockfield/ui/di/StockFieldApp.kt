2012deb3460334a265f2495f4b3e483ddfd0cb4c
SunGuk <sgk0713@gmail.com>
2021-10-04 18:10:21 +0900

    firebase initialize 추가

--- app/src/main/java/com/evangers/stockfield/ui/di/StockFieldApp.kt
+++ app/src/main/java/com/evangers/stockfield/ui/di/StockFieldApp.kt
@@ -2,6 +2,7 @@ package com.evangers.stockfield.ui.di
 
 import android.app.Application
 import com.google.android.gms.ads.MobileAds
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -9,6 +10,7 @@ class StockFieldApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        FirebaseApp.initializeApp(this)
         MobileAds.initialize(this) {}
     }
 }
