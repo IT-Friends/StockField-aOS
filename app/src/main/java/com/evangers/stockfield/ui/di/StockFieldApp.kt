975e16fa8ad95f4baf8bb12905d5a3fc1f8c6793
SunGuk <sgk0713@gmail.com>
2021-10-09 13:59:33 +0900

    테스트용 로그 삽입

--- app/src/main/java/com/evangers/stockfield/ui/di/StockFieldApp.kt
+++ app/src/main/java/com/evangers/stockfield/ui/di/StockFieldApp.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.evangers.stockfield.domain.usecase.SetInitOpenTime
 import com.google.android.gms.ads.MobileAds
 import com.google.firebase.FirebaseApp
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -19,11 +20,17 @@ class StockFieldApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        FirebaseCrashlytics.getInstance().recordException(Throwable("app-1"))
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        FirebaseCrashlytics.getInstance().recordException(Throwable("app-2"))
         FirebaseApp.initializeApp(this)
+        FirebaseCrashlytics.getInstance().recordException(Throwable("app-3"))
         MobileAds.initialize(this) {}
+        FirebaseCrashlytics.getInstance().recordException(Throwable("app-4"))
         CoroutineScope(Dispatchers.Default).launch {
+            FirebaseCrashlytics.getInstance().recordException(Throwable("app-5"))
             setInitOpenTime(System.currentTimeMillis())
+            FirebaseCrashlytics.getInstance().recordException(Throwable("app-6"))
         }
     }
 }
